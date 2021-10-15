8039bb2c48a35643bf5df2d38417d559cb81d7ee
EhoodGoren <ehoodgoren@gmail.com>
2021-10-15 23:13:16 +0300

    Images are now displayed properly

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const baseURL = "https://pokeapi.co/api/v2/";
 // identifier is name or id
 async function getPokemon(identifier){
-    const response = await axios.get(`${baseURL}pokemon/ditto`).catch((error) => {
+    if (typeof(identifier) === "string"){
+        identifier = identifier.toLowerCase();
+    }
+    identifier = identifier.toLowerCase();
+    const response = await axios.get(`${baseURL}pokemon/${identifier}`).catch((error) => {
         throw error
     });
     return response.data;
@@ -12,8 +16,8 @@ function pokemonInfo(pokemon){
     const newPokemon = {
         height: pokemon.height,
         weight: pokemon.weight,
-        img: pokemon.image,
-        back: pokemon.backImg
+        img: pokemon.sprites.front_default,
+        back: pokemon.sprites.back_default
     }
     return newPokemon;
 }
@@ -21,21 +25,32 @@ function pokemonInfo(pokemon){
 // generates a new pokemon in DOM
 // pokemon = obj
 function generatePokemon(pokemon){
-    console.log(pokemon);
     const newPokemon = document.createElement("div");
     for(let attribute in pokemon){
-        console.log(pokemon[attribute])
-        const newAttribute = document.createElement("div");
-        newAttribute.innerText = `${attribute}: ${pokemon[attribute]}`;
+        const newAttribute = checkAttribute(pokemon, attribute)
         newPokemon.appendChild(newAttribute);
     }
     const resultArea = document.getElementById("selected_pokemon");
     resultArea.appendChild(newPokemon);
 }
 
+function checkAttribute(pokemon, attribute){
+    if(attribute === "img" || attribute === "back"){
+        const newImg = document.createElement("img");
+        newImg.setAttribute("src", pokemon[attribute]);
+        return newImg;
+    }
+    else{
+        const textAttribtue = document.createElement("div");
+        textAttribtue.innerText = `${attribute}: ${pokemon[attribute]}`;
+        return textAttribtue;
+    }
+}
+
 // identifier is name or id
 async function createPokemon(identifier){
     const pokemonResponse = await getPokemon(identifier);
+    console.log(pokemonResponse)
     const selectedPokemon = pokemonInfo(pokemonResponse);
     generatePokemon(selectedPokemon);
 }
